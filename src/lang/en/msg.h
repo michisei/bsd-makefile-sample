76abd23bc67670bb0fe682f442e787cde011f475
michisei <daozheng99+2310@hotmail.com>
2025-05-02 20:13:04 +0800

    Added test source files.

---
+++ src/lang/en/msg.h
@@ -0,0 +1,6 @@
+#ifndef MSG_H_INCLUDED
+#define MSG_H_INCLUDED
+
+extern char* msg;
+
+#endif // MSG_H_INCLUDED
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "lang/en/msg.h"
+
+int main(void) {
+	(void) printf("%s\n", msg);
+	return 0;
+}
